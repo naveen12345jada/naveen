06d27827669fdfc372f535a2cd2612ea768fc60f
JADA NAVEEN KUMAR <jadnavee@publicisgroupe.net>
2022-07-11 11:06:09 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,140 @@
+let javascriptIsFun=true;
+console.log(typeof javascriptIsFun );
+console.log(40+24+34);
+console.log("naveen");
+let firstName="naveen";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+console.log(typeof 'naveen');
+console.log(typeof 23);
+console.log(typeof true)
+javascriptIsFun="naveen";
+console.log(typeof javascriptIsFun );
+ let a =24;
+
+let naveen ;
+console.log(typeof naveen );
+naveen="naveen kumar"
+console.log(typeof naveen );
+console.log(typeof null);
+ a= 23;
+console.log(a);
+const birthYear=1991;
+var w= 20;
+w=21;
+console.log(w);
+lastName ='jada';
+console.log(lastName)
+lastName ='naveen';
+console.log(lastName)
+const ageNaveen=2021-1999;
+const ageNavee=2021-1998;
+console.log(ageNaveen *2 ,ageNavee/2, 2 ** 3)
+
+
+/*let marksmass=78;
+let marksheight=1.69;
+markBMI=marksmass/(marksheight*marksheight);
+console.log(markBMI);
+
+let johnsmass=92;
+let johnsheight=1.95;
+johnsBMI=johnsmass/(johnsheight*johnsheight);
+console.log(johnsBMI);
+
+ismarkHigherBMI=markBMI > johnsBMI
+console.log(ismarkHigherBMI);
+*/
+
+
+
+
+
+
+firstName='jada naveen kumar';
+dateofbirth=1999;
+now=2022;
+age=2022-1999;
+job='teacher';
+
+ console.log("I'am "+firstName + age +" year's"+'  old '+job+"!");
+naveen=`I'am ${firstName}  ${age} year's old ${job} ! `;
+console.log(naveen);
+
+// normal string
+console.log("hi\n\
+naveen\n\
+kumar");
+// template literals or backtics
+
+console.log(`naveen
+kumar
+jada`);
+
+// code driving license
+
+const ag = 16;
+
+if (ag>=18){
+    console.log("naveen  is eligible for driving ")
+}
+else{
+    lessage=18-ag;
+    console.log("naveen  is  not eligible for driving and he was"+" "+lessage+" "+"years less than the eligible age");
+    console.log(`naveen  is  not eligible for driving and he was ${lessage} years less than the eligible age`);
+}
+
+let marksmass=92;
+let marksheight=1.95;
+markBMI=marksmass/(marksheight*marksheight);
+console.log(markBMI);
+
+let johnsmass=78;
+let johnsheight=1.69;
+johnsBMI=johnsmass/(johnsheight*johnsheight);
+console.log(johnsBMI);
+
+if(markBMI > johnsBMI){
+    console.log(`mark's bmi is ${markBMI} is higher than the ${johnsBMI} johns bmi`);
+}
+else{
+    console.log(`john's bmi is ${johnsBMI} is higher than the ${markBMI} marks bmi`);
+}
+
+const year="1991";
+console.log(Number(year),year);
+console.log(Number(year)+9);
+console.log(Number("naveen"));
+console.log(typeof NaN);
+var d=23
+console.log(String(d));
+
+/*console.log("I am + 23 + years old ")
+console.log( "23"-"10")
+console.log( "23"-10)
+console.log( "23"/"10")
+console.log( "23"*"10") */
+
+n="1"+1;
+n=n+1;
+n=n-1;
+console.log(n);
+console.log("23"-1-2+"3");
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('jonas'));
+console.log(Boolean({}));
+console.log(Boolean(''));
+  
+const money =0;
+if (money)
+{
+    console.log('money has ');
+}else{
+    console.log('money doesnot have');
+
+}
+
+                                                      
